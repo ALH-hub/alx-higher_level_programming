05491ab35adc7c9ee6c1444694c05d71d0c88be2
Alhadji Oumate <alhadjioum0@gmail.com>
2023-08-29 22:16:20 +0100

    area of the square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """initiate the __size attribute
+        verify if size provided is and integer
+        verify if size provided is less than 0
+
+        Args:
+            size (int): the size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """calculate the area of the square
+
+        return:
+            area of the square
+        """
+        return self.__size ** 2
