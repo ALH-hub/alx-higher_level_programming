b7003c60b06eef750e34251854ed842edc7aa4dd
Alhadji Oumate <alhadjioum0@gmail.com>
2023-08-29 21:58:18 +0100

    private size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+    """empty Square class"""
+    def __init__(self, size):
+        """initiate the __size attribute
+        Args:
+            size (int): the size of the square
+        """
+        self.__size = size
