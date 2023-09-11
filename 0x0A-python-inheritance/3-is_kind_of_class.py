405e5915cdee532dc3f8481f6807fad87cfd1c13
Alhadji Oumate <alhadjioum0@gmail.com>
2023-09-11 15:03:45 +0100

    inheritance verification

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""defines the is_kind_of_class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checks if the object is an instance of or if object
+        is an instance of a class that inherited from
+    Args:
+        obj (object): object of a class
+        a_class (class): class
+    Return:
+        True if object is an instance or if object is an instance of a class inherited from
+        False otherwise
+    """
+    if type(obj) == a_class or issubclass(type(obj), a_class):
+        return True
+    return False
