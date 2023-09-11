7fbbe3d7e7ee5b0603ee828fb08e5bba9ba2bf7e
Alhadji Oumate <alhadjioum0@gmail.com>
2023-09-11 13:57:38 +0100

    attribute and methods of a class list

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,14 +5,4 @@
 
 def lookup(obj):
     """returns the list of available attributes and methods of an object"""
-    attribute = []
-    method = []
-
-    for item in dir(obj):
-        if not item.startswith('__'):
-            if callable(getattr(obj, item)):
-                method.append(item)
-            else:
-                attribute.append(item)
-
-    return attribute + method
+    return dir(obj)
