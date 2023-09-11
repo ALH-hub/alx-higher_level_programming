5906ca7e07ee91fb7fb1fc10f9d106fa68b468e6
Alhadji Oumate <alhadjioum0@gmail.com>
2023-09-11 14:20:50 +0100

    list inheritace

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""defines the print_sorted class"""
+
+
+class MyList(list):
+    """list class to print in ascending order"""
+    def print_sorted(self):
+        """print a list in sorted order"""
+        sorted_list = self[:]
+        sorted_list.sort()
+        print(sorted_list)
