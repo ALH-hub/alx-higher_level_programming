d7c0a5e87d8eb8c68ff71fbf06e279e646cc24bd
Alhadji Oumate <alhadjioum0@gmail.com>
2023-09-12 22:54:35 +0100

    from json file to object

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define load_from_json_file function"""
+import json
+
+
+def load_from_json_file(filename):
+    """load the json string from a json file
+    Args:
+        filename (string): json file name
+    Returns:
+        the object representation of the json file content
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.loads(f.read())
