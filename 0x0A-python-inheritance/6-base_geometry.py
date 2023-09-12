e03498f404ddcf319222ad5212cdab3b1431c241
Alhadji Oumate <alhadjioum0@gmail.com>
2023-09-12 01:27:53 +0100

    not implemented method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Base geometry class"""
+
+    def area(self):
+        """not implemented method"""
+        raise Exception("area() is not implemented")
