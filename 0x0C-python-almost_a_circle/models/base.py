c9e8bd76f23c429ca4c1ba7cd06719d1e2cd8807
Alhadji Oumate <alhadjioum0@gmail.com>
2023-11-13 20:34:13 +0100

    tasks complete

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""define the Base class"""
+import json
+
+
+class Base:
+    """the base classes"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialize the id of the class
+        Args:
+            id (int): the id provided as parameter
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """return the json serialized dictionaries
+        Args:
+            list_dictionaries (list): list of dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file
+        Args:
+            list_objs (list): list of Base intances children
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation
+        Args:
+            json_string (str): string representing a list of dictionaries
+        Returns:
+            empty list if json_string is None or empty
+            Otherwise list represented by json_string
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """that returns an instance with all attributes already set
+        Args:
+            **dictionary (dict): pairs of attributes to initialize
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances
+        Returns:
+            empty list if the file does not exist
+            Otherwise list of instantiated classes
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
---
+++ 0x0C-python-almost_a_circle/tests/test_models/__pycache__/Untitled-1.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+int main() {
+    printf("here're im i\n");
+}
