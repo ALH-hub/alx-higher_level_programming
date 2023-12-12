50f1fb0163631806e2a5150fe456579dc0a5a153
Alhadji Oumate <alhadjioum0@gmail.com>
2023-12-12 23:47:07 +0100

    completed

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- computes the score average of records in table second_table of database hbtn_0c_0
+
+SELECT AVG(score) AS average
+FROM second_table;
---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- lists the number of records with same score in table second_table of database hbtn_0c_0
+
+SELECT score, COUNT(*) AS `number`
+FROM second_table
+GROUP BY score
+ORDER BY `number` DESC;
